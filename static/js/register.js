85441ba3f90f3898db076bc0eae4573cc53332e3
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-12-12 09:20:12 +0545

    add register.js code

--- static/js/register.js
+++ static/js/register.js
@@ -3,11 +3,22 @@ let password = document.getElementsByTagName("input")[1];
 let button = document.getElementsByTagName("button")[0];
 
 button.addEventListener("click", () => {
+	let options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			username: username.value,
+			password: password.value
+		})
+	}
 
+	registerUser(options)
 });
 
-let registerUser = async function() {
-	let res = await fetch("http://localhost:8080/register", );
+let registerUser = async function(options) {
+	let res = await fetch("http://localhost:8080/register", options);
 	let data = res.json;
 }
 
